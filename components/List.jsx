9a6b9d34dce463b3c2ff2cea993b09a9d7526a56
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-03-29 20:50:13 +0530

    List Comp

---
+++ components/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function List() {
+  return (
+    <div>
+      <div>
+        <h2>Title</h2>
+        <h4>Description</h4>
+      </div>
+    </div>
+  )
+}
+
+export default List
