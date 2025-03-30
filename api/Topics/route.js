f204424723df72db0c91fd22dea5060a76f75602
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-03-30 19:33:40 +0530

    Setup API endpoint

---
+++ api/Topics/route.js
@@ -0,0 +1,10 @@
+import Topic from '@/models/topic';
+import connectMongoDB from '@/libs/mongodb';
+import { NextResponse } from 'next/server';
+
+export async function POST (request) {
+  const { title, description } = await request.json();
+  await connectMongoDB();
+  await Topic.create({ title, description });
+  return NextResponse.json({ message: 'Topic created' }, { status: 201 });
+}
--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-const connectMongoDB = () => {
+const connectMongoDB = async () => {
   try {
-    mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
