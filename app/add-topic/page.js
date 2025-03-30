af95202cb0aeedbc3b8010b058f7723b826b87ce
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-03-30 19:15:12 +0530

    Add topic SSR

--- app/add-topic/page.js
+++ app/add-topic/page.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function AddTopic() {
   return (
-    <div>AddTopic</div>
+    <form className='flex flex-col gap-2'>
+      <input className='border border-slate-400 mt-2 p-3 mx-5' type='text' placeholder='Title' required/>
+      <input className='border border-slate-400 mt-2 p-3 mx-5' type='text' placeholder='Description' />
+      <button className='bg-green-400 text-white p-3 hover:cursor-pointer w-fit mx-auto rounded'>Add Topic</button>
+    </form>
   )
 }
 
