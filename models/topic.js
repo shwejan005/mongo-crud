28be850fa6bb4747a1e0ce1c10503366d8157840
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-03-30 19:30:15 +0530

    Defined Schema

---
+++ models/topic.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from "mongoose";
+
+const topicSchema = new Schema(
+  {
+    title: String,
+    desciption: String
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const Topic = mongoose.models.Topic || mongoose.model("Topic", topicSchema);
+
+export default Topic;
