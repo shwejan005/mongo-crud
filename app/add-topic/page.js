4137bc9e7691d5623e3b3fae0637a0972234c50f
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-04-02 20:30:05 +0530

    Handle SSR of data

--- app/add-topic/page.js
+++ app/add-topic/page.js
@@ -1,10 +1,17 @@
+'use client'
 import React from 'react'
 
 function AddTopic() {
+  const [title, settitle] = useState("");
+  const [desc, setdesc] = useState("")
+  function handleSubmit (e) {
+    e.preventDefault();
+    if (!title || !desc) alert("Please fill all the fields");
+  }
   return (
     <form className='flex flex-col gap-2'>
-      <input className='border border-slate-400 mt-2 p-3 mx-5' type='text' placeholder='Title' required/>
-      <input className='border border-slate-400 mt-2 p-3 mx-5' type='text' placeholder='Description' />
+      <input onChange={(e)=> settitle(e.target.value)} className='border border-slate-400 mt-2 p-3 mx-5' type='text' placeholder='Title' required/>
+      <input onChange={(e)=> settitle(e.target.desc)} className='border border-slate-400 mt-2 p-3 mx-5' type='text' placeholder='Description' />
       <button className='bg-green-400 text-white p-3 hover:cursor-pointer w-fit mx-auto rounded'>Add Topic</button>
     </form>
   )
--- components/List.jsx
+++ components/List.jsx
@@ -3,20 +3,45 @@ import Remove from './Remove'
 import Link from 'next/link'
 import { Edit } from 'lucide-react'
 
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Topic", {
+      cache: "no-store",
+    });
 
-function List() {
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
+};
+
+
+export default async function List() {
+  const { topics } = await getTopics();
+
   return (
-    <div className='p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start'>
-      <div>
-        <h2 className='font-bold text-2xl'>Title</h2>
-        <h4>Description</h4>
-      </div>
-      <div className='flex gap-2'>
-        <Remove />
-        <Link href={'/edit-topic'}><Edit /></Link>
-      </div>
-    </div>
-  )
-}
-
-export default List
+    <>
+      {topics.map((t) => (
+        <div
+          key={t._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <div>{t.description}</div>
+          </div>
+          <div className="flex gap-2">
+            <Remove id={t._id}/>
+            <Link href={`/edit-topic/${t._id}`}>
+              <Edit size={24}/>
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
